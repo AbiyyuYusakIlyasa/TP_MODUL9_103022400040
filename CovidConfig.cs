3a0e8e2f7a7ba263e5cb275e74376e2152225705
Yusak Abiyyu <yusakabiyyu@gmail.com>
2026-04-27 00:43:44 +0700

    Menambahkan Implementasi Runtime Configuration

---
+++ CovidConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+public class CovidConfig
+{
+    public string satuan_suhu { get; set; }
+    public int batas_hari_demam { get; set; }
+    public string pesan_ditolak { get; set; }
+    public string pesan_diterima { get; set; }
+
+    public static CovidConfig LoadConfig()
+    {
+        string path = "covid_config.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<CovidConfig>(json);
+        }
+        else
+        {
+            return new CovidConfig
+            {
+                satuan_suhu = "celcius",
+                batas_hari_demam = 14,
+                pesan_ditolak = "Anda tidak diperbolehkan masuk ke dalam gedung ini",
+                pesan_diterima = "Anda dipersilahkan untuk masuk ke dalam gedung ini"
+            };
+        }
+    }
+}
+    
--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CovidConfig config = CovidConfig.LoadConfig();
+        Console.WriteLine("DEBUG: " + config.satuan_suhu);
+
+        Console.WriteLine($"Berapa suhu badan anda saat ini? Dalam nilai {config.satuan_suhu}: ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Berapa hari yang lalu (perkiraan) anda terakhir memiliki gejala demam?");
+        int hariDemam = Convert.ToInt32(Console.ReadLine());
+
+        bool suhuNormal;
+
+        if (config.satuan_suhu.ToLower() == "celcius")
+        {
+            suhuNormal = suhu >= 36.5 && suhu <= 37.5;
+        }
+        else
+        {
+            suhuNormal = suhu >= 97.7 && suhu <= 99.5;
+        }
+
+        if (suhuNormal && hariDemam < config.batas_hari_demam)
+        {
+            Console.WriteLine(config.pesan_diterima);
+        }
+        else
+        {
+            Console.WriteLine(config.pesan_ditolak);
+        }
+    }
+}
